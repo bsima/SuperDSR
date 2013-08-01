b77a297de59e95cf7a8ba92b36a4885a1439ad6c
bsima <bensima@gmail.com>
2013-07-31 22:28:48 -0400

    Working

--- assets/js/_init.js
+++ assets/js/_init.js
@@ -7,19 +7,21 @@
  *
 */
 
-/* @function currentURL
+/* @function getURL
  * @purpose Simply grabs the URL and sets it as a variable that can be regex'd
  *
  * @TODO Verify that this function works!
 */
 
-var currentURL;
+var currentURL; // Needs to be a global variable.
 
 function getURL(regex) {
 
-  var href = window.location.href;
+  // Specific to Chrome. Get current tab URL and store in variable tablink
+  var tablink;
+  chrome.tabs.getSelected(null,function(tab) { var tablink = tab.url; });
 
-  currentURL = new String(href);
+  currentURL = new String(tablink);
 
   if ( regex != null ) {
 
