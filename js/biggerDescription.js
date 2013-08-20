0f650122d6ae57874a0e2209d82a771e8b5742b3
Ben Sima <bensima@gmail.com>
2013-08-20 11:06:50 -0400

    Fixed part of  Issue 3 - biggerDescription.js works.

--- js/biggerDescription.js
+++ js/biggerDescription.js
@@ -11,19 +11,7 @@
  * @package Footprints
 */
 
-
-// Regex that selects for Footprints ticket pages only
-var re = /.*footprints.*.main.ad.rit.edu/MRcgi/MRTicketPage.pl.*/g;
-
-// Execute only if the URL matches the regex
-if ( window.location.href.match(re) ) {
-
-  var allDescsFrame = document.getElementsByName("ALL_DESCS")[0]; // Get the description box
-  allDescsFrame.style.width = "836px"; // Set the width
-  allDescsFrame.style.minWidth= "400px"; // Set the minimum width
-  allDescsFrame.height = 800; // set the height
-
-  // I'm not sure if this is even necessary... whatever it still needs to be coverted to jQuery
-  // @TODO Convert this line to jQuery. And figure out in general how to edit CSS with jQuery.
-  GM_addStyle("#descrpt_tbl{width:75%;min-width:400px;}");
-};
+var allDescsFrame = document.getElementsByName("ALL_DESCS")[0]; // Get the description box
+allDescsFrame.style.width = "836px"; // Set the width
+allDescsFrame.style.minWidth= "400px"; // Set the minimum width
+allDescsFrame.height = 800; // set the height
